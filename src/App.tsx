befc3ed0d1342722878d882732162a9fde964eec
FuzzyFade <1339184537@qq.com>
2020-09-09 14:58:59 +0800

    🔥delete: 删除动画带来的报错

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Root, Routes, Head } from 'react-static'
 import { Router } from '@reach/router'
 import { GeistProvider } from '@geist-ui/react'
-import { Transition, animated } from 'react-spring/web.cjs'
+// import { Transition, animated } from 'react-spring/web.cjs'
 import LoadingScreen from 'components/LoadingScreen'
 import './app.styl'
 
@@ -21,22 +21,22 @@ const App: React.FC = () => {
             <Router>
               <Routes
                 path="*"
-                render={({ routePath, getComponentForPath }) => {
-                  const element = getComponentForPath(routePath);
-                  return (
-                    <Transition
-                      native
-                      items={routePath}
-                      from={{ transform: 'translateY(-100px)', opacity: 0 }}
-                      enter={{ transform: 'translateY(0px)', opacity: 1 }}
-                      leave={{ transform: 'translateY(-100px)', opacity: 0 }}
-                    >
-                      {() => (props: any) => {
-                        return <animated.div style={props}>{element}</animated.div>
-                      }}
-                    </Transition>
-                  )
-                }}
+                // render={({ routePath, getComponentForPath }) => {
+                //   const element = getComponentForPath(routePath);
+                //   return (
+                //     <Transition
+                //       native
+                //       items={routePath}
+                //       from={{ transform: 'translateY(-100px)', opacity: 0 }}
+                //       enter={{ transform: 'translateY(0px)', opacity: 1 }}
+                //       leave={{ transform: 'translateY(-100px)', opacity: 0 }}
+                //     >
+                //       {() => (props: any) => {
+                //         return <animated.div style={props}>{element}</animated.div>
+                //       }}
+                //     </Transition>
+                //   )
+                // }}
               />
             </Router>
           </React.Suspense>
