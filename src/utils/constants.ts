b89f9f858dbab30c83fdb431f241e28c8c7a18cb
Kid <44045911+kidonng@users.noreply.github.com>
2022-07-13 02:14:04 +0800

    Improve (#4)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,2 @@
-import arch from 'arch'
-
-export const ANDROID_DOWNLOAD_URL = arch() === 'x64' ?
-  'https://incu-download.ncuos.com/iNCU_latest.apk' :
-  'https://incu-download.ncuos.com/iNCU_latest_arm32.apk'
-
-export const IOS_DOWNLOAD_URL = 'https://apps.apple.com/cn/app/%E5%8D%97%E5%A4%A7%E5%AE%B6%E5%9B%AD/id1209726561'
+export const IOS_DOWNLOAD_URL =
+    'https://apps.apple.com/cn/app/%E5%8D%97%E5%A4%A7%E5%AE%B6%E5%9B%AD/id1209726561'
