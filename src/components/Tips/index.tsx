f0881e63ac1a8865ab60fe9eebcf27b8bbe7d4d0
Kuss <kussssss@outlook.com>
2022-10-10 19:55:26 +0800

    feat: show Tip on non-native browser

--- src/components/Tips/index.tsx
+++ src/components/Tips/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "./index.module.styl";
+import { isAlipay, isDingTalk, isWeChat, isWeCom } from 'check-platform'
+import { UA } from 'check-platform/es/Platform'
+
+const isQQ = Boolean(UA) && UA.indexOf('qq') > 0;
 
 export default () => {
-  return <div className={styles.container}>若无法自动跳转<br></br>请在浏览器打开本页面 ⬆️</div>;
+  if (isAlipay || isDingTalk || isWeChat || isWeCom || isQQ) {
+    return <div className={styles.container}>若无法自动跳转<br></br>请在浏览器打开本页面 ⬆️</div>;
+  }
+  return null
 };
